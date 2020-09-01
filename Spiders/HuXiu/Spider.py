d56815a3d18d42cde574c0c38bf3ff27239dc97e
panzy25 <pzy000301@gmail.com>
2020-09-01 14:39:02 +0800

    update codes

--- Spiders/HuXiu/Spider.py
+++ Spiders/HuXiu/Spider.py
@@ -10,9 +10,17 @@ import os
 import time
 import logging
 
+trapInfo = {}
+hotKeyWordsPath = 'C:\\Users\\Tommy Pan\\Desktop\\huxiu\\hotKeyword.txt'
+
 '''
     用于实现虎嗅网的相关爬取工作
     To crawl articles on huxiu.com
+'''
+
+'''
+    断点续爬通过更新配置文件实现
+    如果断点续爬使用队列的形式实现
 '''
 
 # 得到文章网址后获得网页静态内容
@@ -30,8 +38,16 @@ def getArticleContent(articleURL):
 # 对静态文章网页进行解析，返回含标题，时间和内容的字典
 def processContent(content):
     soup = BeautifulSoup(content, 'html.parser')
-    articleContent = soup.find('div', attrs = "article-content").get_text()
-    articleTitle = soup.find('div', attrs = "article-content-title-box").find('div', attrs = "title").get_text()
+    try:
+        articleContent = soup.find('div', attrs = "article-content").get_text()
+        articleTitle = soup.find('div', attrs = "article-content-title-box").find('div', attrs = "title").get_text()
+    except AttributeError as e:
+        print(e)
+        return {
+            'title': None,
+            'content': None,
+            'time': None
+        }
     # 静态网页的时间戳位置不确定，容错
     if soup.find('div', attrs = "m-article-time") != None:
         articleTime = soup.find('div', attrs = "m-article-time").get_text()
@@ -77,6 +93,11 @@ def getSearchLink(url, formData):
         return articleLinkList
     except Exception as e:
         print(e)
+        trapInfo = {
+            'keyword': formData['s'],
+            'page': formData['page']
+        }
+        return trapInfo
         print('Failed to keyword visit ' + url)
     except Exception as e:
         print(e)
@@ -84,10 +105,13 @@ def getSearchLink(url, formData):
 
 # 写入到指定路径，根据文章相关信息进行写入
 def writeToDisk(path, articleInfo):
+    if articleInfo['content'] == None:
+        return
     with open(path, 'a', encoding='utf-8') as f:
         f.write(str(articleInfo['title']) + '\n' +str(articleInfo['time']) + '\n' + str(articleInfo['content']) + '\n')
         f.close()
 
+# 获取热搜词条
 def getHotKeyWords():
     hotURL = 'https://article-api.huxiu.com/tag/hot'
     formData = {
@@ -98,6 +122,7 @@ def getHotKeyWords():
     hotKeyWordsList = hotKeyWordsJson['data']
     return hotKeyWordsList
 
+# 对获得的指令进行基于频道内容的循环爬取
 def crawlOnChannel(channelURL, channelFormData, path):
     while (True):
         articleLinkList = getStreamLink(channelURL, channelFormData)
@@ -105,32 +130,61 @@ def crawlOnChannel(channelURL, channelFormData, path):
             channelFormData['last_time'] = articleLinkList['last_time']
             for articleLink in articleLinkList['articleLinks']:
                 if os.path.exists(path + str(articleLink)[-11:-5] + '.txt'):
+                    print('chongfu')
                     continue
                 else: writeToDisk(path + str(articleLink)[-11:-5] + '.txt', processContent(getArticleContent(articleLink)))
         except TypeError as e:
             print('Stream Ended.')
             break
 
+# 对获得的指令进行基于热搜词条的循环爬取
 def crawlBySearch(searchURL, searchFormData, path):
-    hotKeyWordsList = getHotKeyWords()
+    hotKeyWordsList = updateKeywords()
     for hotKeyWord in hotKeyWordsList:
-        # searchFormData['s'] = '电商' # s是检索内容
-        searchFormData['s'] = hotKeyWord
+        searchFormData['s'] = hotKeyWord['keyword']
+        searchFormData['page'] = hotKeyWord['page']
         while (True):
             articleLinkList = getSearchLink(searchURL, searchFormData)
-            print('Search for: ' + hotKeyWord + ' page: ' + str(searchFormData['page']))
+            print('Search for: ' + hotKeyWord['keyword'] + ' page: ' + str(searchFormData['page']))
+
+            # 做容错处理，如果收到的是429报错或者该检索词检索结束做结束循环处理
             if articleLinkList == []:
+                with open(hotKeyWordsPath, 'r') as f:
+                    hotKeywordJson = json.loads(f.read())
+                    f.close()
+                for hotKeyWordInfo in hotKeywordJson['awaiting']:
+                    if hotKeyWordInfo['keyword'] == hotKeyWord['keyword']:
+                        hotKeywordJson['awaiting'].remove(hotKeyWordInfo)
+                        break
+                hotKeywordJson['finished'].append(hotKeyWord['keyword'])
+                with open(hotKeyWordsPath, 'w+') as f:
+                    f.write(json.dumps(hotKeywordJson, ensure_ascii=False))
+                    f.close()
                 break
-            elif articleLinkList == None:
+            elif type(articleLinkList) == dict:
+                print('Trapped in point: keyword = ' + str(articleLinkList['keyword']) + ' ,page = ' + str(articleLinkList['page']))
+                with open(hotKeyWordsPath,'r') as f:
+                    hotKeywordJson = json.loads(f.read())
+                    f.close()
+                for hotKeyWordInfo in hotKeywordJson['awaiting']:
+                    if hotKeyWordInfo['keyword'] == articleLinkList['keyword']:
+                        hotKeyWordInfo['page'] = articleLinkList['page']
+                        break
+                with open(hotKeyWordsPath, 'w+') as f:
+                    f.write(json.dumps(hotKeywordJson, ensure_ascii=False))
+                    f.close()
                 break
+
             for articleNum in articleLinkList:
                 articleURL = 'https://m.huxiu.com/article/' + str(articleNum) + '.html'
-                if os.path.exists(path + hotKeyWord + str(articleNum) + '.txt'):
+                if os.path.exists(path + hotKeyWord['keyword'] + str(articleNum) + '.txt'):
+                    print(path + hotKeyWord['keyword'] + str(articleNum) + '.txt has existed.')
                     continue
                 else:
-                    writeToDisk(path + hotKeyWord + str(articleNum) + '.txt',processContent(getArticleContent(articleURL)))
+                    writeToDisk(path + hotKeyWord['keyword'] + str(articleNum) + '.txt',processContent(getArticleContent(articleURL)))
             searchFormData['page'] = searchFormData['page'] + 1
 
+# 进行热搜爬取的指令
 def crawlJob_search():
     path = 'C:\\Users\\Tommy Pan\\Desktop\\huxiu\\search\\'
     # 用于使用检索词的爬取(包含热门检索词的获取)
@@ -140,28 +194,72 @@ def crawlJob_search():
         'page': 1,  # page是翻页指标
         'pagesize': 20  # pagesize不会影响page的翻页，即使超过也能访问
     }
+    # if trapInfo != {}:
+    #     searchFormData['s'] = trapInfo['keyword']
+    #     searchFormData['page'] = trapInfo['page']
 
     # 关键词检索爬取
     crawlBySearch(searchURL, searchFormData, path)
 
+# 进行频道内容的爬取指令
 def crawlJob_Stream():
     path = 'C:\\Users\\Tommy Pan\\Desktop\\huxiu\\stream\\'
     # 用于使用栏目分类的爬取
     channelURL = 'https://article-api.huxiu.com/web/channel/articleList'
     channelFormData = {
         'platform': 'www',
-        'last_time': '1597894020',
+        'last_time': '1597852200',
         'channel_id': '105',
         'pagesize': '22'
     }
 
     crawlOnChannel(channelURL, channelFormData, path)  # 栏目流爬取
 
+def updateKeywords():
+    hotKeywordsList = getHotKeyWords()
+    print(hotKeywordsList)
+    # 读取热搜数据
+    with open(hotKeyWordsPath, 'r') as f:
+        hotKeywordJson = json.loads(f.read())
+        f.close()
+
+    # 更新热搜爬取数据
+    for hotKeyword in hotKeywordsList:
+        exit_flag = False
+        for hotKeywordInfo in hotKeywordJson['awaiting']:
+            if hotKeyword == hotKeywordInfo['keyword']:
+                # 已存在等候队列
+                exit_flag = True
+                break
+        if hotKeyword in hotKeywordJson['finished']:
+            # 已完成，根据时间追加爬取？后续优化
+            continue
+        elif exit_flag == True:
+            # 已存在于等候队列
+            continue
+        else:
+            hotKeywordInfo = {
+                'keyword': hotKeyword,
+                'page': 1
+            }
+            hotKeywordJson['awaiting'].append(hotKeywordInfo)
+            with open(hotKeyWordsPath, 'w+') as tmp_f:
+                tmp_f.write(json.dumps(hotKeywordJson, ensure_ascii = False))
+                tmp_f.close()
+
+    # 读取更新后的数据，返回当前需要爬取的检索词
+    with open(hotKeyWordsPath, 'r') as f:
+        hotKeywordJson = json.loads(f.read())
+        hotKeyWordsList = hotKeywordJson['awaiting']
+        f.close()
+        return hotKeyWordsList
+
+
 if __name__ == '__main__':
     # 创建调度器：BlockingScheduler
     scheduler = BlockingScheduler()
     # 添加任务,时间间隔20min
-    scheduler.add_job(crawlJob_search, 'interval', minutes = 20, id = 'crawlJob_search')
+    scheduler.add_job(crawlJob_search, 'interval', minutes = 30, id = 'crawlJob_search')
     # 添加任务,时间间隔20min
-    scheduler.add_job(crawlJob_Stream, 'interval', minutes = 20, id = 'crawlJob_stream')
+    # scheduler.add_job(crawlJob_Stream, 'interval', hours = 1, id = 'crawlJob_stream')
     scheduler.start()
--- Spiders/leiphoneSpider.py
+++ Spiders/leiphoneSpider.py
@@ -6,30 +6,66 @@ import json
 from bs4 import BeautifulSoup
 
 def getStreamLink(url):
+    print('Pull Request to ' + url)
     response = requests.get(url)
     try:
         response.raise_for_status()
         response.encoding = 'utf-8'
         return response.text
-    except:
-        print('HTTPError: Request Failed.')
+    except requests.HTTPError as e:
+        print(e)
+        print('HTTPError: Request for Article Web Failed.')
 
 # 返回存有每篇文章字典信息的列表集合
 def processStream(streamContent):
-    streamContentJson = json.loads(streamContent)
-    streamContentSoup = BeautifulSoup(streamContentJson['html'], 'html.parser')
+    streamContentSoup = BeautifulSoup(streamContent, 'html.parser')
     articleLinkInfoList = []
-    for articleDiv in streamContentSoup.find_all('li'):
-        #print(articleDiv.find('a', attrs = 'headTit').attrs)
+    for articleDiv in streamContentSoup.find('ul', attrs = "clr").find_all('li'):
         articleDivLink = articleDiv.find('a', attrs = 'headTit').attrs
         articleLinkInfo = {
             'link': articleDivLink['href'], # link键对应文章的URL
             'title': articleDivLink['title'] # title键对应文章的标题
         }
         articleLinkInfoList.append(articleLinkInfo)
-    print(articleLinkInfoList)
+    for articleLinkInfo in articleLinkInfoList:
+        print(articleLinkInfo['link'] + ' ' + articleLinkInfo['title'])
     return articleLinkInfoList
 
+def getRequestText(articleURL):
+    print('Pull Request to ' + articleURL)
+    response = requests.get(articleURL)
+    try:
+        response.raise_for_status()
+        response.encoding = 'utf-8'
+        return response.text
+    except requests.HTTPError as e:
+        print(e)
+        print('HTTPError: Request for Article Web Failed.')
+
+def processArticleText(responseText):
+    soup = BeautifulSoup(responseText, 'html.parser')
+    articleContentInfo = {
+        'content': soup.find('div', attrs = "lph-article-comView").get_text(),
+        'time': soup.find('td', attrs = "time").get_text()
+    }
+    return articleContentInfo
+
+def spiderLeiPhone(articleLinkInfoList):
+    articleContentList = []
+    for articleInfo in articleLinkInfoList:
+        articleContentInfo = processArticleText(getRequestText(articleInfo['link']))
+        articleTitle = articleInfo['title']
+        articleInfoDic = {
+            'content' : articleContentInfo['content'],
+            'time' : articleContentInfo['time'],
+            'title' : articleTitle
+        }
+        print(articleInfoDic['title'] + ' ' + articleInfoDic['time'])
+        articleContentList.append(articleInfoDic)
+    return articleContentList
+
 if __name__ == '__main__':
-    activeURL = 'https://www.leiphone.com/site/AjaxLoad/page/1'
-    processStream(getStreamLink(activeURL))
+    activeURL = 'https://www.leiphone.com/category/sponsor/page/1'
+    articleLinkInfoList = processStream(getStreamLink(activeURL))
+    # 带字典的列表
+    spiderLeiPhone(articleLinkInfoList)
